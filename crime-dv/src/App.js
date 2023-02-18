f1333f1e90e6986f9b84e439d0667c30ce8942f2
joerdixon <joseph35dixon@gmail.com>
2023-02-18 15:35:15 -0800

    more styling

--- crime-dv/src/App.js
+++ crime-dv/src/App.js
@@ -30,12 +30,6 @@ function App() {
           <div class='question-container'>
             <div className='question-text'>
               <h1>How has the crime rate changed since 2008?</h1>
-              <h2>Data Used:</h2>
-              <ul>
-                <li>Total Crimes</li>
-                <li>Crimes per Year</li>
-                <li>Crime Type</li>
-              </ul>
             </div>
           </div>
         </Animator>
@@ -43,12 +37,26 @@ function App() {
       {/* Question 2 */}
       <ScrollPage>
         <Animator animation={FadeUp}>
-            <div className='data-vis '>
-              <p>
-                Crimes over Time:
-              </p>
-              <img src="https://via.placeholder.com/500" alt='placeholder image for data vis' />
+          <div className='data-vis '>
+            {/* Data Visualization */}
+            <figure>
+              {/* Caption */}
+              <p>Crimes over Time:</p>
+              {/* Visualizaiton */}
+              <img className='visual' src="https://via.placeholder.com/500" alt='placeholder image for data vis' />
+            </figure>
+            {/* Conclusions/Observations */}
+            <div>
+              <h1>Observations:</h1>
+              <ul>
+                <li>Ballard saw the most sharp increase in crime</li>
+                <li>Ballard saw the most sharp increase in crime</li>
+                <li>Ballard saw the most sharp increase in crime</li>
+                <li>Ballard saw the most sharp increase in crime</li>
+                <li>Ballard saw the most sharp increase in crime</li>
+              </ul>
             </div>
+          </div>
         </Animator>
       </ScrollPage>
       {/* Question 3*/}
@@ -59,6 +67,15 @@ function App() {
             <p>Covid Increased Crime</p>
             <p>Ballard's crime increased the most</p>
             <p>Theft rose by 300%</p>
+          </div>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage>
+        <Animator animation={FadeUp}>
+          <div className='conclusions'>
+            <iframe>
+
+            </iframe>
           </div>
         </Animator>
       </ScrollPage>
